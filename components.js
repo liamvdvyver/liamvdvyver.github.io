1af13f88c9883f5af146ec293d235feb8044f0fd
Liam van der Vyver <91105007+liamvdvyver@users.noreply.github.com>
2026-04-20 11:02:56 +1000

    feat: add webring

--- components.js
+++ components.js
@@ -2,10 +2,12 @@ class NavLinks extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <div class=row-links>
+            <div><a href="https://ring.liampas.ca/left">\<\<</a></div>
             <div><a href="index.html">home</a></div>
             <div><a href="https://github.com/liamvdvyver">github</a></div>
             <div><a href="https://linkedin.com/in/liam-van-der-vyver-2b21b5274">linkedin</a></div>
             <div><a href="https://instagram.com/_lvdv">'gram</a></div>
+            <div><a href="https://ring.liampas.ca/right">\>\></a></div>
         </div>
         `;
     }
